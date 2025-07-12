2cc9d8e33759b307d4e64682a39d9c93b63c8f13
ashishrahi <148324855+ashishrahi@users.noreply.github.com>
2025-07-12 23:04:41 +0530

    updated code

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
-// export const BASE_URL = "https://server-xfik.onrender.com/v1/api";
+export const BASE_URL = "https://server-xfik.onrender.com/v1/api";
 
-export const BASE_URL = "http://localhost:3001/v1/api";
+// export const BASE_URL = "http://localhost:3001/v1/api";
 
 
 export const API_ENDPOINTS = {
